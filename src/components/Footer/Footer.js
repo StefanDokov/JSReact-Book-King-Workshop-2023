40db3789879b113e25618b21f1a0b081c57a2ab6
StefanDokov <stefandokov2211@gmail.com>
2023-03-27 17:16:03 +0300

    remove

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import footerstyle from './footerstyle.module.css';
-
-export const Footer = () => {
-    return(
-        <footer className={footerstyle.footer}>
-        <p>&copy; Book King 2023</p>
-      </footer>
-    )
-}
